76aaa89524ec70db98af7f4c3f8589f026cec6f6
Math2 <Math2@DESKTOP-HD5Q5DJ>
2022-09-03 23:11:47 -0300

    Adicionar arquivos de projeto.

---
+++ Aula2 - Profª Kátia - RA.6322092/Filme.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula2___Profª_Kátia___RA._6322092
+{
+    internal class Filme
+    {
+        string titulo;
+        string sinopse;
+        string genero;
+        string clasificacao;
+
+        public void Receber(string ti, string si, string ge, string cl)
+        {
+            this.titulo = ti;
+            this.sinopse = si;
+            this.genero = ge;
+            this.clasificacao = cl;
+
+        }
+        public string Mostrar()
+        {
+            return "Titulo = " + this.titulo +
+                "\nSinopse = " + this.sinopse +
+                "\nGênero = " + this.genero +
+                "\nclasificação = " + this.clasificacao;
+        }
+
+    }
+}
